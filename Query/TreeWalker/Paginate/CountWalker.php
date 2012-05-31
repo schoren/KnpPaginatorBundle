c60299ad59b3d9b6a8bfd674454c770d23ed9cab
Sebastian Choren <schoren@codigoaustral.com>
2012-05-31 12:18:51 -0300

    blah

--- Query/TreeWalker/Paginate/CountWalker.php
+++ Query/TreeWalker/Paginate/CountWalker.php
@@ -33,7 +33,7 @@ class CountWalker extends TreeWalkerAdapter
     /**
      * Distinct mode hint name
      */
-    const HINT_PAGINATOR_COUNT_DISTINCT = 'bundle.knp_paginator.distinct';
+    const HINT_PAGINATOR_COUNT_DISTINCT = 'knp_paginator.distinct';
 
     /**
      * Walks down a SelectStatement AST node, modifying it to retrieve a COUNT
@@ -43,15 +43,22 @@ class CountWalker extends TreeWalkerAdapter
      */
     public function walkSelectStatement(SelectStatement $AST)
     {
-        $parent = null;
-        $parentName = null;
+        $rootComponents = array();
         foreach ($this->_getQueryComponents() AS $dqlAlias => $qComp) {
-            if ($qComp['parent'] === null && $qComp['nestingLevel'] == 0) {
-                $parent = $qComp;
-                $parentName = $dqlAlias;
-                break;
+            $isParent = array_key_exists('parent', $qComp)
+                && $qComp['parent'] === null
+                && $qComp['nestingLevel'] == 0
+            ;
+            if ($isParent) {
+                $rootComponents[] = array($dqlAlias => $qComp);
             }
         }
+        if (count($rootComponents) > 1) {
+            throw new \RuntimeException("Cannot count query which selects two FROM components, cannot make distinction");
+        }
+        $root = reset($rootComponents);
+        $parentName = key($root);
+        $parent = current($root);
 
         $pathExpression = new PathExpression(
             PathExpression::TYPE_STATE_FIELD | PathExpression::TYPE_SINGLE_VALUED_ASSOCIATION, $parentName,
